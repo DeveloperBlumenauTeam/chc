583bce6943eef67c74d55bb6504df0f8c33211f4
Eduardo Pagotto <edupagotto@gmail.com>
2019-02-04 22:18:38 -0200

    criado diretorios de base conforme #1

---
+++ teste.py
@@ -0,0 +1,18 @@
+
+#!/usr/bin/env python3
+'''
+Created on 20190204
+Update on 20190204
+@author: Eduardo Pagotto
+'''
+
+#pylint: disable=C0301
+#pylint: disable=C0103
+#pylint: disable=W0703
+#pylint: disable=R0913
+
+
+
+if __name__ == "__main__":
+
+    print("ola mundo!!!!!")
